c2c3a893c5b80db5d6f4d80c0ca141305f11f851
Rėjus <rejusp@wix.com>
2026-06-16 13:25:06 +0300

    Package scripts

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server: {
     // SharedArrayBuffer (required by ffmpeg.wasm) needs cross-origin isolation
     headers: {
